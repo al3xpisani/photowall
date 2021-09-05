36a6272446ab2cbd92196c9b9a6fb79d9b7c4234
al3x <alexandre.pisani.ant@gmail.com>
2021-09-05 14:26:26 -0300

    Update config.js

--- photowall/src/database/config.js
+++ photowall/src/database/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBbMYi-SUDZxhNxSp6zLwFd4dCP4V8oT-w",
+  apiKey: "xxxxxxxxxxxxxxxx",
   authDomain: "photowall-489c0.firebaseapp.com",
-  databaseURL: "https://photowall-489c0-default-rtdb.firebaseio.com",
+  databaseURL: "https://photowallxxxxxxxxxxxx",
   projectId: "photowall-489c0",
   storageBucket: "photowall-489c0.appspot.com",
-  messagingSenderId: "407667172262",
-  appId: "1:407667172262:web:9c98a5f99040e231f02567",
-  measurementId: "G-4F9R1G1182"
+  messagingSenderId: "xxxxx",
+  appId: "1:xxxx",
+  measurementId: "xxxxxxx"
 };
 
 firebase.initializeApp(firebaseConfig)
